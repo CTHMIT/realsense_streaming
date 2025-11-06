d5c12933db63231f3a5a29ddfb805e86fa22b7ad
cthsu <cthsu>
2025-11-06 11:04:43 +0800

    init version

---
+++ src/robot_description/launch/robot_state_publisher.launch.py
@@ -0,0 +1,42 @@
+import os
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
+from launch.substitutions import LaunchConfiguration
+from launch_ros.actions import Node
+import xacro
+
+def generate_launch_description():
+
+    robot_namespace_arg = DeclareLaunchArgument(
+        'namespace',
+        default_value='',
+        description='Robot namespace'
+    )
+
+    # URDF
+    pkg_path = get_package_share_directory('robot_description')
+    xacro_file = os.path.join(pkg_path, 'urdf', 'robot.urdf.xacro')
+    
+    # check xacro 
+    if not os.path.exists(xacro_file):
+        raise Exception(f"URDF file not found at: {xacro_file}")
+
+    # xacro
+    robot_description_config = xacro.process_file(xacro_file)
+    robot_description = robot_description_config.toxml()
+
+    # Robot State Publisher Node
+    robot_state_publisher_node = Node(
+        package='robot_state_publisher',
+        executable='robot_state_publisher',
+        name='robot_state_publisher',
+        namespace=LaunchConfiguration('namespace'),
+        output='screen',
+        parameters=[{'robot_description': robot_description}]
+    )
+
+    return LaunchDescription([
+        robot_namespace_arg,
+        robot_state_publisher_node
+    ])
